b49c38bc78dce519cf47e175312f034409040eec
Shakim Harris <amenraprod@gmail.com>
2021-11-23 05:20:19 -0500

    added first part of memo

---
+++ src/react-hook-lessons/useMemo/Lesson1.jsx
@@ -0,0 +1,27 @@
+import React , { useState } from 'react'; 
+
+const thisFunctionWillSlowDownOurApp = (num1, num2) => {
+    for(let i=0; i < 10000; i++){
+        return num1 + num2;
+    }
+    console.log("This probably took a while");
+}
+
+export default function MemoExample(){
+    const [numToAdd, setNumToAdd] = useState(0);
+    const [num2, setNum2] = useState(0);
+
+    const addedValue = thisFunctionWillSlowDownOurApp(numToAdd);
+    return (
+        <div className="App">
+            <input type="number" 
+            value={numToAdd}
+            onChange={(e) => setNumToAdd(e.target.value)} />
+            <input type="number" 
+            value={num2}
+            onChange={(e => setNum2(e.target.value))} />
+         <div>{addedValue}</div>
+        </div>
+    )
+}
+
