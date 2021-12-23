27c190a20093eb72f49d42f7f830bf1468d09269
Shakim Harris <amenraprod@gmail.com>
2021-12-23 18:40:53 -0500

    sample test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import InputForm from './InputForm';
+import addOrderTotal from "./functions";
 
 
 test('render an input form', () => {
@@ -10,4 +11,9 @@ test('render an input form', () => {
 test('this component has an email', () => {
   let email = "james@yahoo.com";
   expect(email).toBe(email);
+})
+
+test('this order total is added', () => {
+  addOrderTotal(36, 25);
+  expect(addOrderTotal).toBe(addOrderTotal);
 })
---
+++ src/functions.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function addOrderTotal(total:number, tax:number): number {
+    return total + tax;
+}
+
+export default addOrderTotal;
